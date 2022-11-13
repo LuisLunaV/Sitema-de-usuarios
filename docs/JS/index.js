296855f77eb39d4f00a5dc911795dc581f524d6e
LuisLunaV <luis.a.luna.v@outlook.com>
2022-11-13 00:16:57 -0600

    Se elimina la carpeta docs

--- docs/JS/index.js
+++
@@ -1,7 +0,0 @@
-import { formRegistro } from'./registro.js';
-import { formLogin } from './auth.js';
-
-export{
-    formRegistro,
-    formLogin
-}
--- docs/index.js
+++
@@ -1,9 +0,0 @@
-import { formRegistro, formLogin } from './JS/index.js';
-
-const nombrePagina = window.location.pathname.split('/').reverse()[0];
-
-(nombrePagina === 'index.html') ? formLogin() 
-: 
-( nombrePagina === 'registro.html') ? formRegistro()
-:'';
-
